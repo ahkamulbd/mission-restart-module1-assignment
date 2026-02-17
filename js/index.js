cec4492953359d58cb0aaf1a8b68de81fbc0dde0
ahkamul <ahkamulbd@gmail.com>
2026-02-17 12:07:49 +0600

    Why Choose Section shown on UI

--- js/index.js
+++ js/index.js
@@ -1,1 +1,10 @@
 // console.log("Hello Assignment");
+
+const productsUrl = `https://fakestoreapi.com/products`;
+
+const loadProducts = () => {
+    fetch(productsUrl)
+        .then(res => res.json())
+        .then(data => console.log(data))
+}
+loadProducts();
